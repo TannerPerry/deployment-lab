daf4f07eb8f245265902aa80d8c6d26ad6609d99
Tanner Perry <tc-boy@hotmail.com>
2021-10-21 13:48:27 -0600

    added a header

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express ()
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname, '../index.html'));
   });
+  app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './server/index.js'))
+  })
+  app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.css'))
+  })
 
 const port = process.env.PORT || 4567
 
